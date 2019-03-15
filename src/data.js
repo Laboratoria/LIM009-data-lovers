654ba975d5b13c2df526d8fb2f83ae1ddd595294
dajhelyan <assiaxramx10@gmail.com>
2019-03-14 19:53:48 -0500

    funcion que muestra en consola un array de strings con los nombres de los indicadores de un pais

--- src/data.js
+++ src/data.js
@@ -3,8 +3,18 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
-};
+// const peru = indicators.filter(indicator => (indicator.indicatorName));
+// console.log(peru);
 
-window.example = example;
+const filterData = (data) => {
+  const indicatorPer = (data.PER.indicators)
+  const arrNameIndicator = indicatorPer.map(objetIndicator => {
+    const nameIndicator = objetIndicator.indicatorName;
+    return nameIndicator;
+  })
+  console.log(arrNameIndicator);
+};
+ 
+filterData(WORLDBANK);
+
+// window.example = example;
