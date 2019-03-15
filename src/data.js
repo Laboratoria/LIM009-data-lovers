070fae9cd45e9ba15d3af76f24610641c69fcb2e
dajhelyan <assiaxramx10@gmail.com>
2019-03-14 20:09:34 -0500

    funcion filterData que muestra en consola un array de string con los nombres de los indicadores de un pais

--- src/data.js
+++ src/data.js
@@ -1,20 +1,10 @@
-/* Manejo de data */
-
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
-
-// const peru = indicators.filter(indicator => (indicator.indicatorName));
-// console.log(peru);
-
 const filterData = (data) => {
   const indicatorPer = (data.PER.indicators)
   const arrNameIndicator = indicatorPer.map(objetIndicator => {
     const nameIndicator = objetIndicator.indicatorName;
     return nameIndicator;
   })
-  console.log(arrNameIndicator);
+  return arrNameIndicator;
 };
  
-filterData(WORLDBANK);
-
-// window.example = example;
+filterData(WORLDBANK)
