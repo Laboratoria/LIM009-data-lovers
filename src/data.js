eea88f9d478132a766e936de0739e28e09efd65d
dajhelyan <assiaxramx10@gmail.com>
2019-03-20 22:31:04 -0500

    funcion que filtra por por sector solo femenino y muestra en pantalla lista de indicadores filtrados

--- src/data.js
+++ src/data.js
@@ -1,13 +1,18 @@
-
-
 window.WorldBank = {
-    filterDataCountries:(data, country,sector) => {
-    const sectorCodeCountries = (data[country].indicators)
-    const arrSectorCodeIndicator= sectorCodeCountries.filter(objSector => {
-    const SectorCode = objSector.indicatorCode.startsWith(sector);
-    return SectorCode;
-    })
-    return arrSectorCodeIndicator;
+    filterDataCountries: (data, country, sector) => {
+        const sectorCodeCountries = (data[country].indicators);
+        const arrSectorCodeIndicator = sectorCodeCountries.filter(objSector => {
+            const SectorCode = objSector.indicatorCode.startsWith(sector);
+            return SectorCode;
+        })
+        return arrSectorCodeIndicator;
     },
-   }
 
+    filterFemIndicators: (data) => {
+        const genFem = data.filter(filterFem => {
+            const arrFemSector = filterFem.indicatorCode.includes('.FE.');
+            return arrFemSector;
+        })
+        return genFem;
+    },
+}
--- src/main.js
+++ src/main.js
@@ -1,37 +1,16 @@
-
-const data = WORLDBANK;
-const selectCountry = document.getElementById("filtrar-pais")
-let country;
-let result;
-const buttonIndicators= document.getElementById("indicators")
-const selectSector= document.getElementById("filtrar-sector")
-let sector;
-
-selectCountry.addEventListener("click", ()=>{
-country = selectCountry.value;
-console.log(country)}
- );
-
-selectSector.addEventListener("click", ()=>{
-sector= selectSector.value;
-console.log(sector);
- }
- )
-
-
- buttonIndicators.addEventListener("click", ()=>{
-    result = window.WorldBank.filterDataCountries(data,country,sector);
-    console.log(result)
-    let datos = '';
-    const listIndicators = document.getElementById('list-indicator')
-    for(let i = 0; i < result.length; i++){
-      console.log(result[i].indicatorName)
-       datos += `<p>${result[i].indicatorName}</p>`
+const data = window.data;
+const btnIndicators = document.getElementById("indicators");
+btnIndicators.addEventListener('click', () => {
+   const country = document.getElementById("country").value;
+   const sector = document.getElementById("sector").value;
+   let listIndicators = window.WorldBank.filterDataCountries(data, country, sector);
+   let listFemIndicators = window.WorldBank.filterFemIndicators(listIndicators);
+   let datos = '';
+    for(let i = 0; i < listFemIndicators.length; i++){
+       datos += `
+       <ul>
+       <li><a href="#page-data-indicators">${listFemIndicators[i].indicatorName}.</a></li>
+       </ul>`
+      document.getElementById('list-indicator').innerHTML = datos;
     }
-    listIndicators.innerHTML=datos;
-    //return result,
-    })
-
-
-
-
+});
