e5629b00f2ce2b41280f26c67af8717c5fd7d309
dajhelyan <assiaxramx10@gmail.com>
2019-03-21 15:04:50 -0500

    agregando a main condicional que agrega lista de indicadores de 2 sectores que no eran evaluados por el filter

--- src/main.js
+++ src/main.js
@@ -4,13 +4,25 @@ btnIndicators.addEventListener('click', () => {
    const country = document.getElementById("country").value;
    const sector = document.getElementById("sector").value;
    let listIndicators = window.WorldBank.filterDataCountries(data, country, sector);
-   let listFemIndicators = window.WorldBank.filterFemIndicators(listIndicators);
+   let listFemIndicators;
+   if (sector === 'SH' || sector === 'SG'){
+      listFemIndicators = listIndicators
+   } else {
+      listFemIndicators = window.WorldBank.filterFemIndicators(listIndicators);
+   }
+   
    let datos = '';
     for(let i = 0; i < listFemIndicators.length; i++){
-       datos += `
-       <ul>
-       <li><a href="#page-data-indicators">${listFemIndicators[i].indicatorName}.</a></li>
-       </ul>`
-      document.getElementById('list-indicator').innerHTML = datos;
+       datos += `<li id="${listFemIndicators[i].indicatorCode}" class="list">${listFemIndicators[i].indicatorName}.</li>`
+       //datos += '<tr><td>Jill</td><td>Smith</td> <td>50</td></tr>'
     }
+    document.getElementById('list-indicator').innerHTML = datos;
+
+   const datosList = document.querySelectorAll('li.list');
+   Array.from(datosList).forEach(dato => {
+      dato.addEventListener('click', (e) => {
+         // debugger
+         console.log(e.target.id);
+      });
+  });
 });
