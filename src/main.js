05f7ce87aa93fa9c67f0bb127edc38a97ca297fa
Judith18 <judicriss18@gmail.com>
2019-03-23 22:00:03 -0500

    Realizando For in, para el recorrido de los datos,para mostrarlos en una tabla y pintarlo en la pantalla

--- src/main.js
+++ src/main.js
@@ -1,28 +1,41 @@
 const data = window.data;
 const btnIndicators = document.getElementById("indicators");
 const listaIndicators = document.getElementById('list-indicator')
+const tablaDataIndicators=document.getElementById("tabla-data")
 btnIndicators.addEventListener('click', () => {
    const country = document.getElementById("country").value;
    const sector = document.getElementById("sector").value;
    let listIndicators = window.WorldBank.filterDataCountries(data, country, sector);
-   let listFemIndicators = window.WorldBank.filterFemIndicators(listIndicators);
-   let pageDataIndicators=document.getElementById("page-data-indicators")
+   let listFemIndicators;
+   if (sector === 'SH' || sector === 'SG') {
+      listFemIndicators = listIndicators
+   } else {
+      listFemIndicators = window.WorldBank.filterFemIndicators(listIndicators);
+   }
+
    let datos = '';
-    for(let i = 0; i < listFemIndicators.length; i++){
-       datos += 
-       
-      `<li id="${listFemIndicators[i].indicatorName}" class= "list"><a href="#page-data-indicators">${listFemIndicators[i].indicatorName}.</a></li>`
-   };
-    listaIndicators.innerHTML = datos;
-    listaIndicators.querySelectorAll("li.list").forEach((element)=>{
-    element.addEventListener("click",()=>{
-    let dataIndicators = '';
-    for(let i = 0; i < listFemIndicators.length; i++){
-            dataIndicators +=`<p> ${Object.keys(listFemIndicators[i].data)} ${ Object.values(listFemIndicators[i].data)}</p>`
-         pageDataIndicators.innerHTML = dataIndicators;
-         console.log(dataIndicators)
-       }
-    })
+   for (let i = 0; i < listFemIndicators.length; i++) {
+      datos += `<li id="${listFemIndicators[i].indicatorCode}" class="list">${listFemIndicators[i].indicatorName}.</li>`
+      //datos += '<tr><td>Jill</td><td>Smith</td> <td>50</td></tr>'
+   }
+   document.getElementById('list-indicator').innerHTML = datos;
+
+   const datosList = document.querySelectorAll('li.list');
+
+   datosList.forEach(dato => {
+      dato.addEventListener('click', () => {
+         let dataIndividual;
+         const indicatorId = dato.id;
+         const returnIndicatorsData = window.WorldBank.indicatorData(listFemIndicators, indicatorId);
+         for(let i in returnIndicatorsData){
+         dataIndividual +=
+            ` <tr>
+              <td> ${i} </td>
+              <td>${ returnIndicatorsData[i]}</td>
+              </tr>`
+             tablaDataIndicators.innerHTML= dataIndividual;
+             console.log(dataIndividual)
+         }
+      });
    })
 })
-    
