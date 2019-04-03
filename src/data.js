8804379caed58cc4bdc31a5ace2d75d3ff23ceb7
dajhelyan <assiaxramx10@gmail.com>
2019-04-02 22:47:39 -0500

    pruebas

--- src/data.js
+++ src/data.js
@@ -1,10 +1,11 @@
-window.WorldBank = {
+window.worldBank = {
     filterDataCountries: (data, country, sector) => {
         const sectorCodeCountries = (data[country].indicators);
         const arrSectorCodeIndicator = sectorCodeCountries.filter(objSector => {
             const SectorCode = objSector.indicatorCode.startsWith(sector);
             return SectorCode;
         })
+        // console.log(arrSectorCodeIndicator);
         return arrSectorCodeIndicator;
     },
 
@@ -15,18 +16,25 @@ window.WorldBank = {
         })
         return genFem;
     },
-
+    
     indicatorData: (listFemIndicators, indicatorId) => {
-        let arr;
+        let obj = {};
         for (let i = 0; i < listFemIndicators.length; i++) {
             if (listFemIndicators[i].indicatorCode === indicatorId) {
-                arr = listFemIndicators[i].data;
+                obj = listFemIndicators[i].data;
             }
         }
-        return arr;
+        return obj;
     },
 
+    // orderDataTable([], "wer") => {
     orderDataTable: (data, order) => {
+    //     const comparators = {
+    //         asc: (prev, next) => prev[1] - next[1],
+    //         desc: (prev, next) =>  next[1] - prev[1],
+    //     };
+
+    //     return data.sort(comparators[order])
         
         if (order === 'asc') {
             return data.sort((prev, next) => {
@@ -39,7 +47,7 @@ window.WorldBank = {
         }
     },
 
-    averageData: (arrData) => { 
+    averageData: (arrData) => { // Función para calcular el promedio
         let sum = arrData.reduce((previous, current) => previous + current)
         let avgDat = sum / arrData.length;
         return avgDat;
--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ const indicatorsPage = document.getElementById('indicators-page');
 const dataIndicatorsPage = document.getElementById('page-data-indicators');
 const btnIndicators = document.getElementById("indicators");
 const tablaDataIndicators = document.getElementById("tabla-data");
-const tablaEstadist = document.getElementById("tabla-estadist");
+// const tablaEstadist = document.getElementById("tabla-estadist");
 const orderDataBtn = document.getElementById("order-data-btn");
 
 const pages = (pageToShow) => {
@@ -21,6 +21,7 @@ const pages = (pageToShow) => {
 const clickBtnhome = () => {
    pages(homePage);
 };
+//ORIGINALLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL
 
 const clickBtnIndicatorsP = () => {
    pages(indicatorsPage);
@@ -31,7 +32,7 @@ let returnIndicatorsData;
 orderDataBtn.addEventListener('click', () => {
    const selectOrder = document.getElementById('select-order').value;
    const indicators = Object.entries(returnIndicatorsData);
-   const orderedData = window.WorldBank.orderDataTable(indicators, selectOrder);
+   const orderedData = window.worldBank.orderDataTable(indicators, selectOrder);
    const tableHtml = renderPopulationTable(orderedData);
    tablaDataIndicators.innerHTML = tableHtml;
 });
@@ -40,13 +41,14 @@ orderDataBtn.addEventListener('click', () => {
 btnIndicators.addEventListener('click', () => {
    const country = document.getElementById("country").value;
    const sector = document.getElementById("sector").value;
-   let listIndicators = window.WorldBank.filterDataCountries(data, country, sector);
+   let listIndicators = window.worldBank.filterDataCountries(data, country, sector);
    let listFemIndicators = '';
    if (sector === 'SH' || sector === 'SG') {
       listFemIndicators = listIndicators;
    } else {
-      listFemIndicators = window.WorldBank.filterFemIndicators(listIndicators);
+      listFemIndicators = window.worldBank.filterFemIndicators(listIndicators);
    }
+// console.log(listFemIndicators);
 
    let datos = '';
    for (let i = 0; i < listFemIndicators.length; i++) {
@@ -55,7 +57,7 @@ btnIndicators.addEventListener('click', () => {
    document.getElementById('list-indicator').innerHTML = datos;
 
    const datosList = document.querySelectorAll('li.list');
-
+// console.log(datosList);
 
 
 
@@ -67,37 +69,53 @@ btnIndicators.addEventListener('click', () => {
          pages(dataIndicatorsPage);
 
          const indicatorId = dato.id;
-         returnIndicatorsData = window.WorldBank.indicatorData(listFemIndicators, indicatorId);
+         // console.log(typeof indicatorId)
+         returnIndicatorsData = window.worldBank.indicatorData(listFemIndicators, indicatorId);
          const indicators = Object.entries(returnIndicatorsData);
          const tableHtml = renderPopulationTable(indicators);
          tablaDataIndicators.innerHTML = tableHtml;
 
+         // let dataTable = (returnIndicatorsData, returnOrderDataTable) 
+        
 
 
-         let arrayData = Object.values(returnIndicatorsData);
-         let arrayFilterNumberData = arrayData.filter(Number);
-         let minData = Math.min(...arrayFilterNumberData).toFixed(2)
-         let maxData = Math.max(...arrayFilterNumberData).toFixed(2)
-         let promData = window.WorldBank.averageData(arrayFilterNumberData).toFixed(2)
-         let statisticalTable =
-            `<tr> <th>Datos estadisticos</th> <th>Valores</th> </tr> 
-         <tr> <td> Min. </td>
-          <td>${minData}</td>
-         </tr>
-         <tr> <td> Max. </td>
-          <td>${maxData}</td>
-         </tr>
-         <tr> <td> Promedio </td>
-          <td>${promData}</td>
-         </tr>`
-         tablaEstadist.innerHTML = statisticalTable;
-         })   
+         // let arrayData = Object.values(returnIndicatorsData);
+         // let arrayFilterNumberData = arrayData.filter(Number);
+         // let minData = Math.min(...arrayFilterNumberData).toFixed(2)
+         // let maxData = Math.max(...arrayFilterNumberData).toFixed(2)
+         // let promData = window.worldBank.averageData(arrayFilterNumberData).toFixed(2)
+         // let statisticalTable =
+         //    `<tr> <th>Datos estadisticos</th> <th>Valores</th> </tr> 
+         // <tr> <td> Min. </td>
+         //  <td>${minData}</td>
+         // </tr>
+         // <tr> <td> Max. </td>
+         //  <td>${maxData}</td>
+         // </tr>
+         // <tr> <td> Promedio </td>
+         //  <td>${promData}</td>
+         // </tr>`
+         // tablaEstadist.innerHTML = statisticalTable;
+         // })   
       })
    })
-
-
+})
+/*
+  @param data An array of arrays
+*/
 const renderPopulationTable = (data) => {
-    let tableHtml = "";
+   let tableHtml = "";
+   // data.each(([year, percentage]) => {
+   //    if (year.trim() === "" || percentage.trim() === "") {
+   //       return
+   //    }
+
+   //    tableHtml += ` <tr>
+   //          <td>${year}</td>
+   //          <td>${percentage}</td>
+   //       </tr>`
+     
+   // })
 
    for (let i in data) {
      let yearAndPercentage = data[i];
