c5933fd3396d44e7f3a7ec97b96910d9aef1ade8
dajhelyan <assiaxramx10@gmail.com>
2019-04-03 21:02:49 -0500

    test finish

--- src/data.js
+++ src/data.js
@@ -1,57 +1,52 @@
 window.worldBank = {
-    filterDataCountries: (data, country, sector) => {
-        const sectorCodeCountries = (data[country].indicators);
-        const arrSectorCodeIndicator = sectorCodeCountries.filter(objSector => {
-            const SectorCode = objSector.indicatorCode.startsWith(sector);
-            return SectorCode;
-        })
-        // console.log(arrSectorCodeIndicator);
-        return arrSectorCodeIndicator;
-    },
+  filterDataCountries: (data, country, sector) => {
+    const sectorCodeCountries = (data[country].indicators);
+    const arrSectorCodeIndicator = sectorCodeCountries.filter(objSector => {
+      const SectorCode = objSector.indicatorCode.startsWith(sector);
+      return SectorCode;
+    });
+    // console.log(arrSectorCodeIndicator);
+    return arrSectorCodeIndicator;
+  },
 
-    filterFemIndicators: (data) => {
-        const genFem = data.filter(filterFem => {
-            const arrFemSector = filterFem.indicatorCode.includes('.FE');
-            return arrFemSector;
-        })
-        return genFem;
-    },
-    
-    indicatorData: (listFemIndicators, indicatorId) => {
-<<<<<<< HEAD
-        let obj = {};
-        for (let i = 0; i < listFemIndicators.length; i++) {
-            if (listFemIndicators[i].indicatorCode === indicatorId) {
-                obj = listFemIndicators[i].data;
-            }
-=======
-        let arr ='';
-        for(let i = 0; i < listFemIndicators.length; i++){
-           if (listFemIndicators[i].indicatorCode === indicatorId){
-              arr = listFemIndicators[i].data;
-           }
->>>>>>> origin
-        }
+  filterFemIndicators: (data) => {
+    const genFem = data.filter(filterFem => {
+      const arrFemSector = filterFem.indicatorCode.includes('.FE');
+      return arrFemSector;
+    });
+    return genFem;
+  },
+
+  indicatorData: (listFemIndicators, indicatorId) => {
+    let obj = {};
+    for (let i = 0; i < listFemIndicators.length; i++) {
+      if (listFemIndicators[i].indicatorCode === indicatorId) {
+        obj = listFemIndicators[i].data;
+      } else {
         return obj;
-    },
+      }      
+    }
+    return obj;
+  },
 
-    orderDataTable: (returnIndicatorsData, order) => {
-        const arrData = Object.entries(returnIndicatorsData);
-        if (order === 'asc') {
-            arrData.sort((prev, next) => { return prev[1] - next [1];
-            })
-        } else {
-             arrData.sort((prev, next) => {
-                    return next [1] - prev [1];
-                })
-            }
-        return arrData;
-    },
+  orderDataTable: (data, order) => {
+    let arrData = [];
+    if (order === 'asc') {
+      arrData = data.concat().sort((prev, next) => {
+        return prev[1] - next[1];
+      });
+    } else {
+      arrData = data.concat().sort((prev, next) => {
+        return next[1] - prev[1];
+      });
+    }
+    return arrData;
+    // console.log(arrData);
+  },
 
-    averageData: (arrData) => { // Función para calcular el promedio
-            let sum = arrData.reduce((previous, current) =>  previous+current)
-            let avgDat = sum / arrData.length;
-            return avgDat;
-      },
-
-}
+  averageData: (arrData) => { // Función para calcular el promedio
+    let sum = arrData.reduce((previous, current) => previous + current);
+    let avgDat = sum / arrData.length;
+    return avgDat;
+  },
+};
--- src/main.js
+++ src/main.js
@@ -4,189 +4,127 @@ const indicatorsP = document.getElementById('indicators-p');
 const homePage = document.getElementById('home-page');
 const indicatorsPage = document.getElementById('indicators-page');
 const dataIndicatorsPage = document.getElementById('page-data-indicators');
-const btnIndicators = document.getElementById("indicators");
-const tablaDataIndicators = document.getElementById("tabla-data");
-const tablaEstadist = document.getElementById("tabla-estadist");
-const orderDataBtn = document.getElementById("order-data-btn");
-const paintCountry= document.getElementById("select-country2");
-const paintSector= document.getElementById("select-sector2");
-const paintIndicator= document.getElementById("paintIndicator");
+const btnIndicators = document.getElementById('indicators');
+const tablaDataIndicators = document.getElementById('tabla-data');
+const tablaEstadist = document.getElementById('tabla-estadist');
+const orderDataBtn = document.getElementById('order-data-btn');
 
 const pages = (pageToShow) => {
-<<<<<<< HEAD
-   [homePage, indicatorsPage, dataIndicatorsPage].forEach(page => {
-      page.classList.add('hide');
-      page.classList.remove('show')
-   })
-   pageToShow.classList.add('show');
-   pageToShow.classList.remove('hide');
-}
+  [homePage, indicatorsPage, dataIndicatorsPage].forEach(page => {
+    page.classList.add('hide');
+    page.classList.remove('show');
+  });
+  pageToShow.classList.add('show');
+  pageToShow.classList.remove('hide');
+};
 
 const clickBtnhome = () => {
-   pages(homePage);
+  pages(homePage);
 };
-//ORIGINALLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL
+// ORIGINALLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL
 
 const clickBtnIndicatorsP = () => {
-   pages(indicatorsPage);
+  pages(indicatorsPage);
 };
 
 let returnIndicatorsData;
 
 orderDataBtn.addEventListener('click', () => {
-   const selectOrder = document.getElementById('select-order').value;
-   const indicators = Object.entries(returnIndicatorsData);
-   const orderedData = window.worldBank.orderDataTable(indicators, selectOrder);
-   const tableHtml = renderPopulationTable(orderedData);
-   tablaDataIndicators.innerHTML = tableHtml;
+  const selectOrder = document.getElementById('select-order').value;
+  const indicators = Object.entries(returnIndicatorsData);
+  const orderedData = window.worldBank.orderDataTable(indicators, selectOrder);
+  const tableHtml = renderPopulationTable(orderedData);
+  tablaDataIndicators.innerHTML = tableHtml;
 });
 
-=======
-    [homePage, indicatorsPage, dataIndicatorsPage].forEach(page => {
-       page.classList.add('hide');
-       page.classList.remove('show')
-    })
-    pageToShow.classList.add('show');
-    pageToShow.classList.remove('hide');
- }
- 
- const clickBtnhome = () => {
-    pages(homePage);
- };
-
- 
- const clickBtnIndicatorsP = () => {
-    pages(indicatorsPage);
- };
- 
->>>>>>> origin
 
 btnIndicators.addEventListener('click', () => {
-   const country = document.getElementById("country").value;
-   const sector = document.getElementById("sector").value;
-<<<<<<< HEAD
-   let listIndicators = window.worldBank.filterDataCountries(data, country, sector);
-   let listFemIndicators = '';
-=======
-   let listIndicators = window.WorldBank.filterDataCountries(data, country, sector);
-   let listFemIndicators ='';
->>>>>>> origin
-   if (sector === 'SH' || sector === 'SG') {
-      listFemIndicators = listIndicators
-   } else {
-      listFemIndicators = window.worldBank.filterFemIndicators(listIndicators);
-   }
-<<<<<<< HEAD
-// console.log(listFemIndicators);
-=======
->>>>>>> origin
+  const country = document.getElementById('country').value;
+  const sector = document.getElementById('sector').value;
+  let listIndicators = window.worldBank.filterDataCountries(data, country, sector);
+  let listFemIndicators = '';
+  if (sector === 'SH' || sector === 'SG') {
+    listFemIndicators = listIndicators;
+  } else {
+    listFemIndicators = window.worldBank.filterFemIndicators(listIndicators);
+  }
+  // console.log(listFemIndicators);
 
-   let datos = '';
-   for (let i = 0; i < listFemIndicators.length; i++) {
-      datos += `<li id="${listFemIndicators[i].indicatorCode}" class="list">${listFemIndicators[i].indicatorName}.</li>`
-   }
-   document.getElementById('list-indicator').innerHTML = datos;
+  let datos = '';
+  for (let i = 0; i < listFemIndicators.length; i++) {
+    datos += `<li id="${listFemIndicators[i].indicatorCode}" class="list">${listFemIndicators[i].indicatorName}.</li>`;
+  }
+  document.getElementById('list-indicator').innerHTML = datos;
 
-   const datosList = document.querySelectorAll('li.list');
-<<<<<<< HEAD
-// console.log(datosList);
+  const datosList = document.querySelectorAll('li.list');
+  // console.log(datosList);
 
 
+  datosList.forEach(dato => {
+    dato.addEventListener('click', () => {
+      pages(dataIndicatorsPage);
+
+      const indicatorId = dato.id;
+      returnIndicatorsData = window.worldBank.indicatorData(listFemIndicators, indicatorId);
+      const indicators = Object.entries(returnIndicatorsData);
+      const tableHtml = renderPopulationTable(indicators);
+      tablaDataIndicators.innerHTML = tableHtml;
+
+      // let dataTable = (returnIndicatorsData, returnOrderDataTable) 
 
 
-=======
-   let returnIndicatorsData;
->>>>>>> origin
-   datosList.forEach(dato => {
-      dato.addEventListener('click', () => {
-         pages(dataIndicatorsPage);
-         let dataIndividual = "";
-         const indicatorId = dato.id;
-<<<<<<< HEAD
-         // console.log(typeof indicatorId)
-         returnIndicatorsData = window.worldBank.indicatorData(listFemIndicators, indicatorId);
-         const indicators = Object.entries(returnIndicatorsData);
-         const tableHtml = renderPopulationTable(indicators);
-         tablaDataIndicators.innerHTML = tableHtml;
+      let arrayData = Object.values(returnIndicatorsData);
+      let arrayFilterNumberData = arrayData.filter(Number);
+      // console.log(arrayFilterNumberData);
+      let minData = Math.min(...arrayFilterNumberData).toFixed(2);
+      let maxData = Math.max(...arrayFilterNumberData).toFixed(2);
+      let promData = window.worldBank.averageData(arrayFilterNumberData).toFixed(2);
+      let statisticalTable =
+         `<tr> <th>Datos estadisticos</th> <th>Valores</th> </tr> 
+      <tr> <td> Min. </td>
+       <td>${minData}</td>
+      </tr>
+      <tr> <td> Max. </td>
+       <td>${maxData}</td>
+      </tr>
+      <tr> <td> Promedio </td>
+       <td>${promData}</td>
+      </tr>`;
+      tablaEstadist.innerHTML = statisticalTable;
+    });   
+  });
+});
 
-         // let dataTable = (returnIndicatorsData, returnOrderDataTable) 
-=======
-         let labelSelectCountry= document.getElementById("country").selectedIndex;
-         paintCountry.innerHTML=document.getElementsByTagName("option")[labelSelectCountry].label;
-         let labelSelectSector= document.getElementById("sector").selectedIndex;
-         paintSector.innerHTML= document.getElementsByTagName("option")[labelSelectSector].label;
-         paintIndicator.innerHTML= dato;
-         
->>>>>>> origin
-        
-         returnIndicatorsData = window.WorldBank.indicatorData(listFemIndicators, indicatorId);
+/* @param data An array of arrays*/
+const renderPopulationTable = (data) => {
+  let tableHtml = '';
+  // data.each(([year, percentage]) => {
+  //    if (year.trim() === "" || percentage.trim() === "") {
+  //       return
+  //    }
 
-<<<<<<< HEAD
+  //    tableHtml += ` <tr>
+  //          <td>${year}</td>
+  //          <td>${percentage}</td>
+  //       </tr>`
+     
+  // })
 
-         // let arrayData = Object.values(returnIndicatorsData);
-         // let arrayFilterNumberData = arrayData.filter(Number);
-         // let minData = Math.min(...arrayFilterNumberData).toFixed(2)
-         // let maxData = Math.max(...arrayFilterNumberData).toFixed(2)
-         // let promData = window.worldBank.averageData(arrayFilterNumberData).toFixed(2)
-         // let statisticalTable =
-         //    `<tr> <th>Datos estadisticos</th> <th>Valores</th> </tr> 
-         // <tr> <td> Min. </td>
-         //  <td>${minData}</td>
-         // </tr>
-         // <tr> <td> Max. </td>
-         //  <td>${maxData}</td>
-         // </tr>
-         // <tr> <td> Promedio </td>
-         //  <td>${promData}</td>
-         // </tr>`
-         // tablaEstadist.innerHTML = statisticalTable;
-         // })   
-=======
-         for (let i in returnIndicatorsData) {
-            if (returnIndicatorsData[i] !== "") {
-               dataIndividual +=
-                  ` <tr><td> ${i} </td>
-                <td>${ returnIndicatorsData[i].toFixed(2)}</td></tr>`;
-               tablaDataIndicators.innerHTML= dataIndividual;
-               
-            }
-         }
-         orderDataBtn.addEventListener('click', () => {
-            tablaDataIndicators.innerHTML = "";
-            const selectOrder = document.getElementById('select-order').value;
-            let returnOrderDataTable = window.WorldBank.orderDataTable(returnIndicatorsData, selectOrder);
-            let  dataOrderIndividual="";
-            for(let value of returnOrderDataTable){
-               if(value[1] !== "") {
-                  dataOrderIndividual +=
-                     ` <tr><td> ${value[0]} </td>
-                   <td>${ value[1].toFixed(2)}</td></tr>`;
-                  tablaDataIndicators.innerHTML= dataOrderIndividual;
-               }
-         }
-         })    
+  for (let i in data) {
+    let yearAndPercentage = data[i];
+    let year = yearAndPercentage[0];
+    let percentage = yearAndPercentage[1];
+    if (year !== '' || percentage !== '') {
+      const rowHtml = ` <tr>
+            <td>${year}</td>
+            <td>${percentage}</td>
+         </tr>`;
+      tableHtml += rowHtml;
+    }
+  }
+  return tableHtml;
+  // console.log(tableHtml)
+};
 
-         let arrayData = Object.values(returnIndicatorsData);
-         let arrayFilterNumberData = arrayData.filter(Number);
-         let minData = Math.min(...arrayFilterNumberData).toFixed(2)
-         let maxData = Math.max(...arrayFilterNumberData).toFixed(2)
-         let promData = window.WorldBank.averageData(arrayFilterNumberData).toFixed(2)
-         let statisticalTable =
-            `<tr> <th>Datos estadisticos</th> <th>Valores</th> </tr> 
-         <tr> <td> Min. </td>
-          <td>${minData}</td>
-         </tr>
-         <tr> <td> Max. </td>
-          <td>${maxData}</td>
-         </tr>
-         <tr> <td> Promedio </td>
-          <td>${promData}</td>
-         </tr>`
-         tablaEstadist.innerHTML = statisticalTable;
->>>>>>> origin
-      })
-   })
-})
-
-home.addEventListener('click', clickBtnhome)
+home.addEventListener('click', clickBtnhome);
 indicatorsP.addEventListener('click', clickBtnIndicatorsP);
